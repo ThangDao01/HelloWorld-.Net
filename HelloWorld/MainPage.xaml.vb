69fc9302a2234c3941108da62e953e6bb7c82ae4
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-09-21 22:34:27 +0700

    Add project files.

---
+++ HelloWorld/MainPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
